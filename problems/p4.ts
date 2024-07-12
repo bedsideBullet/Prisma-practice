7918d53c2f9dfc6fa447146c5dcc7f10a70e5395
Daniel Smith <danjsmith3@gmail.com>
2024-07-12 16:08:00 -0500

    finished 4

--- problems/p4.ts
+++ problems/p4.ts
@@ -1,4 +1,19 @@
 import { prisma } from "./prisma";
 
 // get All Pg-13 movies, ordered by release year descending
-export const getAllPG13Movies = () => {};
+export const getAllPG13Movies = async () => {
+	const pg13Movies = await prisma.movie.findMany({
+		where: {
+			parentalRating: "PG-13",
+		},
+		orderBy: {
+			releaseYear: "desc",
+		},
+		select: {
+			releaseYear: true,
+			parentalRating: true,
+		},
+	});
+
+	return pg13Movies;
+};
