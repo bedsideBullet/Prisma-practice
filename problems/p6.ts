278f49d2e1050e1dbe14daa9a618473d960f8266
Daniel Smith <danjsmith3@gmail.com>
2024-07-29 16:18:55 -0500

    er

--- problems/p6.ts
+++ problems/p6.ts
@@ -2,9 +2,13 @@ import { prisma } from "./prisma";
 
 // find all movies that a user has watched
 export const findAllMoviesThatAUserWatched = async (userId: number) => {
-	const userMovies = prisma.starRatings.findMany({
-		select: {
-			user: userId,
+	const moviesWatched = await prisma.starRating.findMany({
+		where: {
+			userId: userId,
+		},
+		include: {
+			movie: true,
 		},
 	});
+	return moviesWatched.map((rating) => rating.movie);
 };
--- problems/p7.ts
+++ problems/p7.ts
@@ -1,4 +1,20 @@
 import { prisma } from "./prisma";
 
 // get average score for a user
-export const getAverageScoreForUser = async (userId: number) => {};
+export const getAverageScoreForUser = async (userId: number) => {
+	const movieScores = await prisma.starRating.findMany({
+		where: {
+			userId: userId,
+		},
+		include: {
+			movie: true,
+		},
+	});
+
+	const averageScores = movieScores.map((rating) => rating.score);
+	const totalScore = averageScores.reduce((acc, cur) => acc + cur, 0);
+	const overallAverageScore =
+		averageScores.length > 0 ? totalScore / averageScores.length : 0;
+
+	return overallAverageScore;
+};
